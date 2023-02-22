932ed9ba50b612f4c0a35cf7001b698802e80e6d
Tanish Kumar <75082482+tanishkumar12@users.noreply.github.com>
2023-02-22 13:34:02 +0530

    final with bump

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,9 +10,13 @@ const Cart = (props) => {
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
-    const cartItemRemoveHandler = (id) => { };
+    const cartItemRemoveHandler = (id) => {
+        cartCtx.removeItem(id);
+    };
 
-    const cartItemAddHandler = (item) => { };
+    const cartItemAddHandler = (item) => {
+        cartCtx.addItem({ ...item, amount: 1 });
+    };
 
     const cartItems = (
         <ul className={classes["cart-items"]}>
@@ -37,7 +41,7 @@ const Cart = (props) => {
                 <span>{totalAmount}</span>
             </div>
             <div className={classes.actions}>
-                <button classNmae={classes["button--alt"]} onClick={props.onClose}>
+                <button className={classes["button--alt"]} onClick={props.onClose}>
                     Close
                 </button>
                 {hasItems && <button className={classes.button}>Order</button>}
